f7a34419123ae013ccdc3bae40f58b03a255c545
Justin Mammarella <jmamma@gmail.com>
2018-04-21 19:39:17 +1000

    Added splash screen for OLED

--- avr/cores/megacommand/MCL/MCLGfx.cpp
+++ avr/cores/megacommand/MCL/MCLGfx.cpp
@@ -2,7 +2,30 @@
 #include "MCLGfx.h"
 
 void MCLGfx::splashscreen() {
+#ifdef OLED_DISPLAY
+  oled_display.setTextSize(2);
+  oled_display.setTextColor(WHITE,BLACK);
+  oled_display.setCursor(40, 0);
+  oled_display.println("MEGA");
+  oled_display.setCursor(22, 15);
+  oled_display.println("COMMAND");
+  for (float length = 0; length < 32; length += 0.7) {
 
+    // display.fillRect(0, 0, 128, length, BLACK);
+    for (uint8_t x = 0; x < 50 + (length * 5); x++) {
+
+      oled_display.drawPixel(random(20, 110), 30 - random(0, (int)length),
+                             BLACK);
+    }
+
+    //:    display.drawLine(0, length, 128, length, BLACK);
+
+    oled_display.display();
+  }
+  oled_display.clearDisplay();
+  oled_display.display();
+  oled_display.setTextSize(1);
+#else
   char str1[17] = "MEGACOMMAND LIVE";
   char str2[17] = "V2.x.x";
   str1[16] = '\0';
@@ -12,9 +35,10 @@ void MCLGfx::splashscreen() {
   LCD.puts(str2);
 
   delay(100);
+#endif
   // while (rec_global == 0) {
 
-//  GUI.setPage(&grid_page);
+  //  GUI.setPage(&grid_page);
 }
 
 MCLGfx gfx;
