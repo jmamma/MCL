63e383c8fbe7071e2497cec3bb4725846a3d1a21
Yatao Li <yatli@microsoft.com>
2019-03-29 01:45:57 +0800

    reveng: data collector ready. records req-rsp in records.json for later analytics

---
+++ proto_analyzer/A4Cmds.fs
@@ -0,0 +1,132 @@
+﻿module A4Cmds
+
+let A4_KIT_MESSAGE_ID =                    0x52
+let A4_KIT_REQUEST_ID =                    0x62
+
+let A4_KITX_MESSAGE_ID =                   0x58
+let A4_KITX_REQUEST_ID =                   0x68
+
+let A4_SOUND_MESSAGE_ID =                  0x53
+let A4_SOUND_REQUEST_ID =                  0x63
+
+let A4_SOUNDX_MESSAGE_ID =                 0x59
+let A4_SOUNDX_REQUEST_ID =                 0x69
+
+let A4_PATTERN_MESSAGE_ID =                0x54
+let A4_PATTERN_REQUEST_ID =                0x64
+
+let A4_PATTERNX_MESSAGE_ID =               0x5A
+let A4_PATTERNX_REQUEST_ID =               0x6A
+
+let A4_SONG_MESSAGE_ID =                   0x55
+let A4_SONG_REQUEST_ID =                   0x65
+
+let A4_SONGX_MESSAGE_ID =                  0x5B
+let A4_SONGX_REQUEST_ID =                  0x6B
+
+let A4_SETTINGS_MESSAGE_ID =               0x56
+let A4_SETTINGS_REQUEST_ID =               0x66
+
+let A4_SETTINGSX_MESSAGE_ID =              0x5C
+let A4_SETTINGSX_REQUEST_ID =              0x6C
+
+let A4_GLOBAL_MESSAGE_ID =                 0x57
+let A4_GLOBAL_REQUEST_ID =                 0x67
+
+let A4_GLOBALX_MESSAGE_ID =                0x5D
+let A4_GLOBALX_REQUEST_ID =                0x6D
+
+let A4_ANALOG_CALIBRATION_REQUEST_ID =     0x7C
+
+let A4_PRESS_STOP_KEY_REQUEST_ID =         0xF8
+// BPM does not display LOCK
+let A4_PRESS_STOP_KEY_REQUEST_ID2 =        0xFC
+
+// nothing, but freeze for a while (BPM displays LOCK)
+let A4_UNKNOWN_0_REQUEST_ID =              0xFA
+// nothing, but freeze for a while (BPM displays LOCK)
+let A4_UNKNOWN_1_REQUEST_ID =              0xFB
+// dumps something
+let A4_UNKNOWN_2_REQUEST_ID =              0xFD
+
+(*
+COMMAND 0x60 param
+
+Generates an endless stream of dumps (what is it?)
+*)
+let A4_UNKNOWN_3_REQUEST_ID =              0x60
+
+(*
+E0 00 = pitch down octaves
+E0 01 01 = then, pitch up 2semitones?
+
+E1 = track2
+E2 = track3
+E3 01 = track4 down 2 semitones
+...
+E8 same
+*)
+let A4_UNKNOWN_4_REQUEST_ID =              0xE0
+let A4_UNKNOWN_5_REQUEST_ID =              0xE8
+let A4_UNKNOWN_6_REQUEST_ID =              0xE1
+
+// seems to be unmute, 1 param
+let A4_UNKNOWN_7_REQUEST_ID =              0x80 // ~0x84?
+// seems to be mute, 1 param
+let A4_UNKNOWN_8_REQUEST_ID =              0x90 // ~0x94?
+
+let A4_BAD_COMMANDS = [
+    A4_ANALOG_CALIBRATION_REQUEST_ID
+    A4_PRESS_STOP_KEY_REQUEST_ID
+    A4_PRESS_STOP_KEY_REQUEST_ID2
+    A4_UNKNOWN_0_REQUEST_ID
+    A4_UNKNOWN_1_REQUEST_ID
+    // A4_UNKNOWN_2_REQUEST_ID
+    A4_UNKNOWN_3_REQUEST_ID
+    A4_UNKNOWN_4_REQUEST_ID
+    A4_UNKNOWN_5_REQUEST_ID
+    A4_UNKNOWN_6_REQUEST_ID
+    A4_UNKNOWN_7_REQUEST_ID
+    A4_UNKNOWN_8_REQUEST_ID
+]
+
+let A4_KNOWN_COMMANDS = [
+    A4_GLOBALX_MESSAGE_ID
+    A4_GLOBALX_REQUEST_ID
+    A4_GLOBAL_MESSAGE_ID
+    A4_GLOBAL_REQUEST_ID
+    A4_KITX_MESSAGE_ID
+    A4_KITX_REQUEST_ID
+    A4_KIT_MESSAGE_ID
+    A4_KIT_REQUEST_ID
+    A4_PATTERNX_MESSAGE_ID
+    A4_PATTERNX_REQUEST_ID
+    A4_PATTERN_MESSAGE_ID
+    A4_PATTERN_REQUEST_ID
+    A4_SETTINGSX_MESSAGE_ID
+    A4_SETTINGSX_REQUEST_ID
+    A4_SETTINGS_MESSAGE_ID
+    A4_SETTINGS_REQUEST_ID
+    A4_SONGX_MESSAGE_ID
+    A4_SONG_MESSAGE_ID
+    A4_SONGX_REQUEST_ID
+    A4_SONG_REQUEST_ID
+    A4_SOUNDX_MESSAGE_ID
+    A4_SOUNDX_REQUEST_ID
+    A4_SOUND_MESSAGE_ID
+    A4_SOUND_REQUEST_ID
+
+    A4_ANALOG_CALIBRATION_REQUEST_ID
+    A4_PRESS_STOP_KEY_REQUEST_ID
+    A4_PRESS_STOP_KEY_REQUEST_ID2
+    A4_UNKNOWN_0_REQUEST_ID
+    A4_UNKNOWN_1_REQUEST_ID
+    A4_UNKNOWN_2_REQUEST_ID
+    A4_UNKNOWN_3_REQUEST_ID
+    A4_UNKNOWN_4_REQUEST_ID
+    A4_UNKNOWN_5_REQUEST_ID
+    A4_UNKNOWN_6_REQUEST_ID
+    A4_UNKNOWN_7_REQUEST_ID
+    A4_UNKNOWN_8_REQUEST_ID
+]
+
--- proto_analyzer/Program.fs
+++ proto_analyzer/Program.fs
@@ -1,20 +1,54 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+open System.Threading
 open System.IO.Ports
+open A4Cmds
+open Microsoft.FSharp.Text.StructuredFormat
+open Microsoft.FSharp.Text.StructuredFormat.LayoutOps
+open Newtonsoft.Json
 
+let blockL sep_l sep_r (xs: Layout list) =
+    aboveListL [
+    sepL sep_l @@-
+        List.reduce (@@) xs
+    sepL sep_r
+    ]
+
+let pairL k v =
+    wordL k ^^ sepL ":" ^^ objL v
+
+let byteL = sprintf "0x%02x"
+let bytesL xs = sprintf "%A" <| List.map byteL xs
+
+let fmt l = Display.layout_to_string FormatOptions.Default l
+
+[<StructuredFormatDisplay("{Str}")>]
 type CommunicationRecord = 
     {
         cmd:        byte;
         parameters: byte list;
-        send_time:  DateTime;
         response:   byte list;
+        send_time:  DateTime;
         recv_time:  DateTime;
     }
+    with 
+    [<JsonIgnore>]
+    member x.Str = 
+        let layout = blockL "{" "}" [
+            pairL "cmd" (byteL x.cmd)
+            pairL "params" (bytesL x.parameters)
+            pairL "response" (bytesL x.response)
+        ]
+        fmt layout
+
+type CommunicationRecords = CommunicationRecord list
 
 let communicate (com: SerialPort) (input: int list) : CommunicationRecord = 
     match input with
     | [] -> failwith "empty input"
+    | cmd :: _ when List.contains cmd A4_BAD_COMMANDS -> failwith "bad command"
     | cmd :: parameters ->
     let request = 
         [
@@ -40,26 +74,110 @@ let communicate (com: SerialPort) (input: int list) : CommunicationRecord =
     com.Write(request, 0, Array.length request)
 
     // read until first timeout
-    let read_exhaustive () =
-        let rec __read_exhaustive data = 
-            try
-                let x = com.ReadByte() |> byte
-                __read_exhaustive(x :: data)
-            with
-            | :? TimeoutException -> List.rev data
-        __read_exhaustive []
+    let rec __read_exhaustive data = 
+        let read_byte = 
+            try com.ReadByte() |> byte |> Some
+            with | :? TimeoutException -> None
+        match read_byte with
+        | Some x -> __read_exhaustive(x :: data)
+        | None ->
+          {
+            cmd = byte(cmd)
+            parameters = List.map byte parameters
+            send_time = send_time
+            recv_time = DateTime.Now
+            response = List.rev data
+          }
+    __read_exhaustive []
 
-    {
-        cmd = byte(cmd)
-        parameters = List.map byte parameters
-        send_time = send_time
-        recv_time = DateTime.Now
-        response = read_exhaustive()
-    }
+let rng = new System.Random()
+let rand() = rng.Next(0, 128)
+type GenT =
+| Random
+| Fixed of data: int list * editing: int
 
+let synthesize_payload len =
+    [for _ in 1..len -> rand()]
+
+let rec probe com dat cmd len r =
+    let len = max 0 <| min len 6
+    let cmd = max 0 <| min cmd 127
+    let r = match r with
+            | Fixed(dat, _) when List.length dat <> len -> Fixed(synthesize_payload len, 0)
+            | Fixed(dat, i) -> Fixed(dat, max 0 <| min i (List.length dat - 1))
+            | _ -> r
+
+    let request = cmd :: match r with   
+                         | Random -> synthesize_payload len
+                         | Fixed(dat, _) -> dat
+
+    let mk_comm() = 
+        try
+            Console.SetCursorPosition(90, 4)
+            printf "= BUSY ="
+            let record = communicate com request
+            record :: dat
+        with
+        | ex -> 
+            printfn "cmd=0x%02x: %s" cmd ex.Message
+            dat
+
+    Console.Clear()
+    let history = List.truncate 5 dat
+    history |> List.iter (printfn "%A")
+    Console.SetCursorPosition(90, 0)
+    printf "Current cmd = 0x%02x; Len = %A" cmd len
+    Console.SetCursorPosition(90, 1)
+    printf "Current dat = "
+    let ith_pos i = 104 + i * 3
+    request |> List.iteri (fun i x -> Console.SetCursorPosition(ith_pos i, 1); printf "%02x" x)
+
+    match r with
+    | Fixed(_, i) ->
+      Console.SetCursorPosition(2 + ith_pos i+2, 2)
+      printf("^")
+    | _ -> ()
+
+    let probe' = probe com dat
+    let k = Console.ReadKey().Key
+
+    Thread.Sleep(10)
+
+    let plus  i i' x = if i <> i' then x else x+1
+    let minus i i' x = if i <> i' then x else x-1
+
+    match k, r with
+    | ConsoleKey.Escape, _        -> dat
+    | ConsoleKey.Enter, _         -> probe com (mk_comm()) cmd len r
+    | ConsoleKey.PageDown, _      -> probe' (cmd+1) len r
+    | ConsoleKey.PageUp, _        -> probe' (cmd-1) len r
+    | ConsoleKey.Home, _          -> probe' cmd (len-1) r
+    | ConsoleKey.End, _           -> probe' cmd (len+1) r
+    | ConsoleKey.Spacebar, Random -> probe' cmd len <| Fixed(synthesize_payload len, 0)
+    | ConsoleKey.Spacebar, Fixed _-> probe' cmd len Random
+    | _, Fixed(dat, i) ->
+      let dat, i = 
+          match k with
+          | ConsoleKey.UpArrow    -> List.mapi (plus i)  dat, i
+          | ConsoleKey.DownArrow  -> List.mapi (minus i) dat, i
+          | ConsoleKey.LeftArrow  -> dat, i-1
+          | ConsoleKey.RightArrow -> dat, i+1
+          | _                     -> dat, i
+      probe' cmd len <| Fixed(dat, i)
+    | _                      -> probe' cmd len r
+               
+    
 [<EntryPoint>]
 let main argv =
     use com = new SerialPort("COM3", 250000)
     com.ReadTimeout <- 100
+    com.Open()
+
+    let results = 
+        probe com [] 0x64 1 Random
+        |> List.map JsonConvert.SerializeObject
+
+    File.AppendAllLines("records.json", results)
     
+
     0 // return an integer exit code
