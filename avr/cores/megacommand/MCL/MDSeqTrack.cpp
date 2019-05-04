54191191f997428843520546487813da1747cffa
Justin Mammarella <jmamma@gmail.com>
2019-05-04 18:12:05 +1000

    Bug Fix (tentative): Paramter lock sequences would change when a second paramater recorded.
    Do not transmit (kit orig) value for parameter locks when lock mask is
    set, but no param value is locked.

--- avr/cores/megacommand/MCL/MDSeqTrack.cpp
+++ avr/cores/megacommand/MCL/MDSeqTrack.cpp
@@ -150,11 +150,6 @@ void MDSeqTrack::send_parameter_locks(uint8_t step) {
     for (c = 0; c < 4; c++) {
       if (locks[c][step] > 0) {
         MD.setTrackParam(track_number, locks_params[c] - 1, locks[c][step] - 1);
-      } else if (locks_params[c] > 0) {
-        MD.setTrackParam(track_number, locks_params[c] - 1,
-                         locks_params_orig[c]);
-        //        MD.setTrackParam(track_number, locks_params[c] - 1,
-        //                 MD.kit.params[track_number][locks_params[c] - 1]);
       }
     }
   } else if (IS_BIT_SET64(pattern_mask, step)) {
