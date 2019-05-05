85108bc99e0c261fb21ea960e9e3e1ff9f463b52
Justin Mammarella <jmamma@gmail.com>
2019-05-05 12:47:18 +1000

    Encoders were unresponsive for Mute/Cue pages

--- avr/cores/megacommand/MCL/AuxPages.cpp
+++ avr/cores/megacommand/MCL/AuxPages.cpp
@@ -4,6 +4,9 @@ extern MCLEncoder mixer_param1(0, 127);
 extern MCLEncoder mixer_param2(0, 127);
 extern MCLEncoder mixer_param3(0, 127);
 extern MCLEncoder mixer_param4(0, 127);
+
+extern MCLEncoder mute_param1(0, 7);
+
 MixerPage mixer_page(&mixer_param1, &mixer_param2, &mixer_param3, &mixer_param4);
-CuePage cue_page(&mixer_param1, &mixer_param2, &mixer_param3);
-MutePage mute_page(&mixer_param1, &mixer_param2, &mixer_param3);
+CuePage cue_page(&mute_param1, &mute_param1, &mute_param1);
+MutePage mute_page(&mute_param1, &mute_param1, &mute_param1);
