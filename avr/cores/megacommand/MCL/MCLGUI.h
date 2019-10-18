52fef1a0fd1911f8c6f0e63be6217e6f1c15e8e7
Yatao Li <yatli@microsoft.com>
2019-10-18 14:29:50 +0800

    implement question dialog box

--- avr/cores/megacommand/MCL/MCLGUI.h
+++ avr/cores/megacommand/MCL/MCLGUI.h
@@ -4,11 +4,13 @@
 #define MCLGUI_H__
 
 #include "TextInputPage.h"
+#include "QuestionDialogPage.h"
 
 class MCLGUI {
 public:
   bool wait_for_input(char *dst, const char *title, uint8_t len);
-  void draw_infobox(const char* line1, const char* line2);
+  bool wait_for_confirm(const char *title, const char* text);
+  void draw_infobox(const char* line1, const char* line2, const int line2_offset = 0);
   void draw_vertical_dashline(uint8_t x);
   void draw_vertical_separator(uint8_t x);
   void draw_vertical_scrollbar(uint8_t x, uint8_t n_items, uint8_t n_window, uint8_t n_current);
@@ -23,6 +25,16 @@ public:
   static constexpr uint8_t s_menu_y = (32 - s_menu_h) / 2;
   static constexpr uint8_t s_title_x = 31;
   static constexpr uint8_t s_title_w = 64;
+
+  static constexpr auto info_y1 = 2;
+  static constexpr auto info_y2 = 27;
+  static constexpr auto info_x1 = 12;
+  static constexpr auto info_x2 = 124;
+  static constexpr auto circle_x = info_x1 + 10;
+  static constexpr auto circle_y = info_y1 + 15;
+
+  static constexpr auto info_w = info_x2 - info_x1 + 1;
+  static constexpr auto info_h = info_y2 - info_y1 + 1;
 
 };
 
