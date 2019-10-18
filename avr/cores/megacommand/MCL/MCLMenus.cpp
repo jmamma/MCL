a615cac56ca15aa25fd207d0dd61fb2ac3480b55
Yatao Li <yatli@microsoft.com>
2019-10-18 20:28:29 +0800

    remove ram_config_page dup

--- avr/cores/megacommand/MCL/MCLMenus.cpp
+++ avr/cores/megacommand/MCL/MCLMenus.cpp
@@ -13,7 +13,6 @@ MCLEncoder config_param6(0, 17, ENCODER_RES_SYS);
 MCLEncoder config_param7(0, 17, ENCODER_RES_SYS);
 
 MenuPage aux_config_page(&auxconfig_menu_layout, &config_param1, &config_param6);
-MenuPage ram_config_page(&rampage1_menu_layout, &config_param1, &config_param6);
 MenuPage system_page(&system_menu_layout, &options_param1, &options_param2);
 MenuPage midi_config_page(&midiconfig_menu_layout, &config_param1,
                           &config_param3);
