6589ed3f4a9d0651967e63ca871ad690c1ec70f1
Yatao Li <yatli@microsoft.com>
2019-10-19 19:50:17 +0800

    menu mask implementation ok...

--- avr/cores/megacommand/MCL/Menu.h
+++ avr/cores/megacommand/MCL/Menu.h
@@ -35,18 +35,24 @@ class Menu {
 public:
   menu_t *layout;
   uint8_t values[MAX_MENU_ITEMS];
+  uint8_t entry_mask[2];
 
-  Menu() {}
+  Menu(){
+      entry_mask[0] = entry_mask[1] = 0xFF;
+  }
 
   void set_layout(menu_t *menu_layout);
+  void enable_entry(uint8_t entry_index, bool en);
+  bool is_entry_enable(uint8_t entry_index);
 
   PGM_P get_name();
 
   uint8_t get_number_of_items();
   menu_item_t *get_item(uint8_t item_n);
+  uint8_t get_item_index(uint8_t item_n);
   PGM_P get_item_name(uint8_t item_n);
   Page *get_page_callback(uint8_t item_n);
-  //Page *get_exit_page_callback();
+  // Page *get_exit_page_callback();
   FP get_exit_function();
   FP get_row_function(uint8_t item_n);
   uint8_t *get_dest_variable(uint8_t item_n);
