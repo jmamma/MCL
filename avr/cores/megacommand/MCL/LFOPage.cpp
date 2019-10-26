fc43cf23ebff500638fd6dcebdf24d0ce7b697e0
Justin Mammarella <jmamma@gmail.com>
2019-10-26 23:13:14 +1100

    Write button now enables + disables LFO

--- avr/cores/megacommand/MCL/LFOPage.cpp
+++ avr/cores/megacommand/MCL/LFOPage.cpp
@@ -1,5 +1,5 @@
-#include "LFOPage.h"
 #include "LFO.h"
+#include "LFOPage.h"
 #include "MCL.h"
 #include "MCLSeq.h"
 
@@ -24,7 +24,7 @@ void LFOPage::init() {
   update_encoders();
 }
 void LFOPage::cleanup() {
-    md_exploit.off();
+  md_exploit.off();
 #ifdef OLED_DISPLAY
   oled_display.clearDisplay();
 #endif
@@ -33,127 +33,126 @@ void LFOPage::cleanup() {
 void LFOPage::update_encoders() {
   if (page_mode == LFO_DESTINATION) {
     encoders[0]->cur = mcl_seq.lfo_tracks[0].params[0].dest;
-    ((MCLEncoder*)encoders[0])->max = NUM_MD_TRACKS + 4;
+    ((MCLEncoder *)encoders[0])->max = NUM_MD_TRACKS + 4;
     encoders[1]->cur = mcl_seq.lfo_tracks[0].params[0].param;
-    ((MCLEncoder*)encoders[1])->max = 23;
+    ((MCLEncoder *)encoders[1])->max = 23;
     encoders[2]->cur = mcl_seq.lfo_tracks[0].params[1].dest;
-    ((MCLEncoder*)encoders[2])->max = 16 + 4;
+    ((MCLEncoder *)encoders[2])->max = 16 + 4;
     encoders[3]->cur = mcl_seq.lfo_tracks[0].params[1].param;
-    ((MCLEncoder*)encoders[3])->max = 23;
+    ((MCLEncoder *)encoders[3])->max = 23;
   }
- if (page_mode == LFO_SETTINGS) {
+  if (page_mode == LFO_SETTINGS) {
     encoders[0]->cur = waveform;
-    ((MCLEncoder*)encoders[0])->max = 2;
+    ((MCLEncoder *)encoders[0])->max = 2;
     encoders[1]->cur = mcl_seq.lfo_tracks[0].mode;
-    ((MCLEncoder*)encoders[1])->max = 2;
+    ((MCLEncoder *)encoders[1])->max = 2;
     encoders[2]->cur = mcl_seq.lfo_tracks[0].speed;
-   ((MCLEncoder*)encoders[2])->max = 127;
+    ((MCLEncoder *)encoders[2])->max = 127;
     encoders[3]->cur = depth;
-    ((MCLEncoder*)encoders[3])->max = 127;
+    ((MCLEncoder *)encoders[3])->max = 127;
   }
 }
 
 void LFOPage::loop() {
- if (page_mode == LFO_DESTINATION) {
+  if (page_mode == LFO_DESTINATION) {
 
-  if (encoders[0]->hasChanged()) {
-    USE_LOCK();
-    SET_LOCK();
-    mcl_seq.lfo_tracks[0].params[0].reset_param_offset();
-    mcl_seq.lfo_tracks[0].params[0].dest = encoders[0]->cur;
-    mcl_seq.lfo_tracks[0].params[0].update_offset();
-    CLEAR_LOCK();
-    if (encoders[0]->cur > NUM_MD_TRACKS) {
-      ((MCLEncoder*)encoders[1])->max = 7;
+    if (encoders[0]->hasChanged()) {
+      USE_LOCK();
+      SET_LOCK();
+      mcl_seq.lfo_tracks[0].params[0].reset_param_offset();
+      mcl_seq.lfo_tracks[0].params[0].dest = encoders[0]->cur;
+      mcl_seq.lfo_tracks[0].params[0].update_offset();
+      CLEAR_LOCK();
+      if (encoders[0]->cur > NUM_MD_TRACKS) {
+        ((MCLEncoder *)encoders[1])->max = 7;
+      } else {
+        ((MCLEncoder *)encoders[1])->max = 23;
+      }
     }
-    else {
-      ((MCLEncoder*)encoders[1])->max = 23;
-    }
-  }
-  if (encoders[1]->hasChanged()) {
-    USE_LOCK();
-    SET_LOCK();
-    mcl_seq.lfo_tracks[0].params[0].reset_param_offset();
-    mcl_seq.lfo_tracks[0].params[0].param = encoders[1]->cur;
-    mcl_seq.lfo_tracks[0].params[0].offset = mcl_seq.lfo_tracks[0].params[0].get_param_offset(encoders[0]->cur, encoders[1]->cur);
-    mcl_seq.lfo_tracks[0].params[0].update_offset();
-    CLEAR_LOCK();
-  }
-
-  if (encoders[2]->hasChanged()) {
-     USE_LOCK();
-    SET_LOCK();
-    mcl_seq.lfo_tracks[0].params[1].reset_param_offset();
-    mcl_seq.lfo_tracks[0].params[1].dest = encoders[2]->cur;
-    mcl_seq.lfo_tracks[0].params[1].update_offset();
-    CLEAR_LOCK();
-    if (encoders[0]->cur > NUM_MD_TRACKS) {
-      ((MCLEncoder*)encoders[3])->max = 7;
-    }
-    else {
-      ((MCLEncoder*)encoders[3])->max = 23;
+    if (encoders[1]->hasChanged()) {
+      USE_LOCK();
+      SET_LOCK();
+      mcl_seq.lfo_tracks[0].params[0].reset_param_offset();
+      mcl_seq.lfo_tracks[0].params[0].param = encoders[1]->cur;
+      mcl_seq.lfo_tracks[0].params[0].offset =
+          mcl_seq.lfo_tracks[0].params[0].get_param_offset(encoders[0]->cur,
+                                                           encoders[1]->cur);
+      mcl_seq.lfo_tracks[0].params[0].update_offset();
+      CLEAR_LOCK();
     }
 
+    if (encoders[2]->hasChanged()) {
+      USE_LOCK();
+      SET_LOCK();
+      mcl_seq.lfo_tracks[0].params[1].reset_param_offset();
+      mcl_seq.lfo_tracks[0].params[1].dest = encoders[2]->cur;
+      mcl_seq.lfo_tracks[0].params[1].update_offset();
+      CLEAR_LOCK();
+      if (encoders[0]->cur > NUM_MD_TRACKS) {
+        ((MCLEncoder *)encoders[3])->max = 7;
+      } else {
+        ((MCLEncoder *)encoders[3])->max = 23;
+      }
+    }
+    if (encoders[3]->hasChanged()) {
+      USE_LOCK();
+      SET_LOCK();
+      mcl_seq.lfo_tracks[0].params[1].reset_param_offset();
+      mcl_seq.lfo_tracks[0].params[1].param = encoders[3]->cur;
+      mcl_seq.lfo_tracks[0].params[1].offset =
+          mcl_seq.lfo_tracks[0].params[1].get_param_offset(encoders[2]->cur,
+                                                           encoders[3]->cur);
+      mcl_seq.lfo_tracks[0].params[1].update_offset();
+      CLEAR_LOCK();
+    }
   }
-  if (encoders[3]->hasChanged()) {
-    USE_LOCK();
-    SET_LOCK();
-    mcl_seq.lfo_tracks[0].params[1].reset_param_offset();
-    mcl_seq.lfo_tracks[0].params[1].param = encoders[3]->cur;
-    mcl_seq.lfo_tracks[0].params[1].offset = mcl_seq.lfo_tracks[0].params[1].get_param_offset(encoders[2]->cur, encoders[3]->cur);
-    mcl_seq.lfo_tracks[0].params[1].update_offset();
-    CLEAR_LOCK();
-  }
+  if (page_mode == LFO_SETTINGS) {
+    if (encoders[0]->hasChanged()) {
+      waveform = encoders[0]->cur;
+      load_wavetable(waveform, &mcl_seq.lfo_tracks[0], depth);
+    }
+    if (encoders[1]->hasChanged()) {
+      mcl_seq.lfo_tracks[0].mode = encoders[1]->cur;
+    }
 
+    if (encoders[2]->hasChanged()) {
+      mcl_seq.lfo_tracks[0].speed = encoders[2]->cur;
+    }
 
-  }
- if (page_mode == LFO_SETTINGS) {
-  if (encoders[0]->hasChanged()) {
-    waveform = encoders[0]->cur;
-    load_wavetable(waveform, &mcl_seq.lfo_tracks[0], depth);
-  }
-  if (encoders[1]->hasChanged()) {
-    mcl_seq.lfo_tracks[0].mode = encoders[1]->cur;
-  }
-
-  if (encoders[2]->hasChanged()) {
-    mcl_seq.lfo_tracks[0].speed = encoders[2]->cur;
-  }
-
-  if (encoders[3]->hasChanged()) {
-    depth = encoders[3]->cur;
-    mcl_seq.lfo_tracks[0].params[0].depth = depth;
-    mcl_seq.lfo_tracks[0].params[1].depth = depth;
-    load_wavetable(waveform, &mcl_seq.lfo_tracks[0], depth);
-  }
-
-
+    if (encoders[3]->hasChanged()) {
+      depth = encoders[3]->cur;
+      mcl_seq.lfo_tracks[0].params[0].depth = depth;
+      mcl_seq.lfo_tracks[0].params[1].depth = depth;
+      load_wavetable(waveform, &mcl_seq.lfo_tracks[0], depth);
+    }
   }
 }
 
-void LFOPage::load_wavetable(uint8_t waveform, LFOSeqTrack *lfo_track, uint8_t depth) {
+void LFOPage::load_wavetable(uint8_t waveform, LFOSeqTrack *lfo_track,
+                             uint8_t depth) {
   SinLFO sin_lfo;
   TriLFO tri_lfo;
   ExpLFO exp_lfo;
   LFO *lfo;
 
   switch (waveform) {
-    case SIN_WAV:
-      lfo = (LFO*) &sin_lfo;
-      lfo_track->offset_behaviour = LFO_OFFSET_CENTRE;
+  case SIN_WAV:
+    lfo = (LFO *)&sin_lfo;
+    lfo_track->offset_behaviour = LFO_OFFSET_CENTRE;
     break;
-    case TRI_WAV:
-      lfo = (LFO*) &tri_lfo;
-      lfo_track->offset_behaviour = LFO_OFFSET_CENTRE;
+  case TRI_WAV:
+    lfo = (LFO *)&tri_lfo;
+    lfo_track->offset_behaviour = LFO_OFFSET_CENTRE;
     break;
-    case EXP_WAV:
-      lfo = (LFO*) &exp_lfo;
-      lfo_track->offset_behaviour = LFO_OFFSET_MAX;
+  case EXP_WAV:
+    lfo = (LFO *)&exp_lfo;
+    lfo_track->offset_behaviour = LFO_OFFSET_MAX;
     break;
   }
-  //ExpLFO exp_lfo(20);
+  // ExpLFO exp_lfo(20);
   for (uint8_t n = 0; n < LFO_LENGTH; n++) {
-  lfo_track->wav_table[n] = (float)lfo->get_sample(n) * ((float)depth * (float) (DIV_1_127));
+    lfo_track->wav_table[n] =
+        (float)lfo->get_sample(n) * ((float)depth * (float)(DIV_1_127));
   }
 }
 void LFOPage::display() {
@@ -185,51 +184,57 @@ void LFOPage::display() {
   oled_display.setCursor(0, 0);
 
   oled_display.print("LFO ");
-  oled_display.print(page_mode ? 1 : 0);
+  if (mcl_seq.lfo_tracks[0].enable) {
+    oled_display.print("ON");
+  } else {
+    oled_display.print("OFF");
+  }
+  // oled_display.print(page_mode ? 1 : 0);
   oled_display.print(" ");
 
-/*  PGM_P param_name = NULL;
-  char str[4];
-  for (uint8_t i = 0; i < GUI_NUM_ENCODERS; i++) {
-    uint8_t n = i + ((page_mode ? 1 : 0) * GUI_NUM_ENCODERS);
+  /*  PGM_P param_name = NULL;
+    char str[4];
+    for (uint8_t i = 0; i < GUI_NUM_ENCODERS; i++) {
+      uint8_t n = i + ((page_mode ? 1 : 0) * GUI_NUM_ENCODERS);
 
-    uint8_t fx_param = params[n].param;
-    uint8_t fx_type = params[n].type;
-    param_name = fx_param_name(fx_type, fx_param);
-    m_strncpy_p(str, param_name, 4);
+      uint8_t fx_param = params[n].param;
+      uint8_t fx_type = params[n].type;
+      param_name = fx_param_name(fx_type, fx_param);
+      m_strncpy_p(str, param_name, 4);
 
-    mcl_gui.draw_light_encoder(30 + 20 * i, 18, encoders[i], str);
+      mcl_gui.draw_light_encoder(30 + 20 * i, 18, encoders[i], str);
 
- //   mcl_gui.draw_md_encoder(30 + 20 * i, 6, encoders[i], str);
-  }
-*/
+   //   mcl_gui.draw_md_encoder(30 + 20 * i, 6, encoders[i], str);
+    }
+  */
 
   uint8_t x = 0;
   uint8_t h = 30;
   uint8_t y = 0;
 
   for (uint8_t n = 0; n < LFO_LENGTH; n++) {
-  oled_display.drawPixel(x + n, (float) 32 - mcl_seq.lfo_tracks[0].wav_table[n] * ((float) h * (float)DIV_1_127), WHITE);
+    oled_display.drawPixel(x + n,
+                           (float)32 - mcl_seq.lfo_tracks[0].wav_table[n] *
+                                           ((float)h * (float)DIV_1_127),
+                           WHITE);
     if (n % 2 == 0) {
       oled_display.drawPixel(x + n, (h / 2) + y, WHITE);
     }
-
   }
   uint8_t i = 0;
   if (page_mode == LFO_DESTINATION) {
 
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DST");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "PAR");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DST");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "PAR");
-
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DST");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "PAR");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DST");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "PAR");
   }
- if (page_mode == LFO_SETTINGS) {
-        mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "SHP");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "TYP");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "SPD");
-       mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DEP");
- }
+  if (page_mode == LFO_SETTINGS) {
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "SHP");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "TYP");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "SPD");
+    mcl_gui.draw_light_encoder(30 + 20 * i, 5, encoders[i++], "DEP");
+  }
 
   draw_pattern_mask();
 
@@ -245,30 +250,30 @@ void LFOPage::draw_pattern_mask() {
   uint8_t seq_w = 5;
   uint8_t trig_h = 5;
 
-    uint64_t pattern_mask = mcl_seq.lfo_tracks[0].pattern_mask;
+  uint64_t pattern_mask = mcl_seq.lfo_tracks[0].pattern_mask;
 
-    uint8_t offset = 0;
-    for (int i = 0; i < 16; i++) {
+  uint8_t offset = 0;
+  for (int i = 0; i < 16; i++) {
 
-      uint8_t idx = i + offset;
-      bool in_range = idx <  mcl_seq.lfo_tracks[0].length;
+    uint8_t idx = i + offset;
+    bool in_range = idx < mcl_seq.lfo_tracks[0].length;
 
-      if (note_interface.notes[i] == 1) {
-        // TI feedback
-        oled_display.fillRect(trig_x - 1, trig_y, seq_w + 2, trig_h + 1, WHITE);
-      } else if (!in_range) {
-        // don't draw
+    if (note_interface.notes[i] == 1) {
+      // TI feedback
+      oled_display.fillRect(trig_x - 1, trig_y, seq_w + 2, trig_h + 1, WHITE);
+    } else if (!in_range) {
+      // don't draw
+    } else {
+      if (IS_BIT_SET64(pattern_mask, i + offset)) {
+        /*If the bit is set, there is a trigger at this position. */
+        oled_display.fillRect(trig_x, trig_y, seq_w, trig_h, WHITE);
       } else {
-        if (IS_BIT_SET64(pattern_mask, i + offset)) {
-          /*If the bit is set, there is a trigger at this position. */
-          oled_display.fillRect(trig_x, trig_y, seq_w, trig_h, WHITE);
-        } else {
-          oled_display.drawRect(trig_x, trig_y, seq_w, trig_h, WHITE);
-        }
+        oled_display.drawRect(trig_x, trig_y, seq_w, trig_h, WHITE);
       }
-
-      trig_x += seq_w + 1;
     }
+
+    trig_x += seq_w + 1;
+  }
 }
 void LFOPage::onControlChangeCallback_Midi(uint8_t *msg) {
   uint8_t channel = MIDI_VOICE_CHANNEL(msg[0]);
@@ -324,10 +329,10 @@ bool LFOPage::handleEvent(gui_event_t *event) {
         SET_BIT64(mcl_seq.lfo_tracks[0].pattern_mask, step);
       } else {
         DEBUG_PRINTLN("Trying to clear");
-        if (clock_diff(note_interface.note_hold,slowclock) < TRIG_HOLD_TIME) {
+        if (clock_diff(note_interface.note_hold, slowclock) < TRIG_HOLD_TIME) {
           CLEAR_BIT64(mcl_seq.lfo_tracks[0].pattern_mask, step);
         }
-      } 
+      }
     }
   }
   if (event->mask == EVENT_BUTTON_RELEASED) {
@@ -337,17 +342,21 @@ bool LFOPage::handleEvent(gui_event_t *event) {
       EVENT_PRESSED(event, Buttons.ENCODER2) ||
       EVENT_PRESSED(event, Buttons.ENCODER3) ||
       EVENT_PRESSED(event, Buttons.ENCODER4)) {
-       GUI.setPage(&grid_page);
+    GUI.setPage(&grid_page);
   }
   if (EVENT_PRESSED(event, Buttons.BUTTON1)) {
     page_mode = !(page_mode);
     update_encoders();
-   }
+  }
 
   if (EVENT_PRESSED(event, Buttons.BUTTON3)) {
   }
 
   if (EVENT_PRESSED(event, Buttons.BUTTON4)) {
+     mcl_seq.lfo_tracks[0].enable = !(mcl_seq.lfo_tracks[0].enable);
+     if (!mcl_seq.lfo_tracks[0].enable) {
+      mcl_seq.lfo_tracks[0].reset_params_offset();
+     }
   }
   if (EVENT_PRESSED(event, Buttons.BUTTON2)) {
     GUI.setPage(&page_select_page);
