316d3e4ab60ecd16654cd265538c7cf99ed2ba19
Yatao Li <yatli@microsoft.com>
2019-11-09 16:14:07 +0800

    revert PageSelectPage changes

--- avr/cores/megacommand/MCL/PageSelectPage.h
+++ avr/cores/megacommand/MCL/PageSelectPage.h
@@ -6,25 +6,21 @@
 #include "GUI.h"
 
 class PageSelectPage : public LightPage {
-public:
-  MCLEncoder enc1;
-  uint8_t page_select;
-  PageSelectPage(Encoder *e1 = NULL, Encoder *e2 = NULL, Encoder *e3 = NULL,
-                 Encoder *e4 = NULL)
-      : LightPage(e1, e2, e3, e4) {
-    encoders[0] = &enc1;
-  }
-
-  virtual void display();
-  virtual void setup();
-  virtual void init();
-  virtual void loop();
-  virtual void cleanup();
-  virtual bool handleEvent(gui_event_t *event);
-
-  LightPage *get_page(uint8_t page_number, char *str = NULL);
-  uint8_t get_nextpage_down();
-  uint8_t get_nextpage_up();
+ public:
+ MCLEncoder enc1;
+ uint8_t page_select;
+ PageSelectPage(Encoder *e1 = NULL, Encoder *e2 = NULL, Encoder *e3 = NULL, Encoder *e4 = NULL) : LightPage( e1, e2, e3 ,e4) {
+   encoders[0] = &enc1;
+ }
+ void display();
+ void setup();
+ void init();
+ void loop();
+ void cleanup();
+ LightPage *get_page(uint8_t page_number, char *str = NULL);
+ uint8_t get_nextpage_down();
+ uint8_t get_nextpage_up();
+ virtual bool handleEvent(gui_event_t *event);
 };
 
 extern PageSelectPage page_select_page;
