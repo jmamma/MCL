f77aee6ad34740809d1ab5ff49498bec51ad2166
Yatao Li <yatli@microsoft.com>
2019-11-15 14:55:12 +0800

    add new icons to page select page

--- avr/cores/megacommand/MCL/PageSelectPage.cpp
+++ avr/cores/megacommand/MCL/PageSelectPage.cpp
@@ -27,9 +27,9 @@ const PageCategory Categories[] PROGMEM = {
 
 const PageSelectEntry Entries[] PROGMEM = {
     {"GRID", &grid_page, 0, 0, 24, 15, (uint8_t *)icon_grid},
-    {"MIXER", &mixer_page, 1, 0, 24, 15, (uint8_t *)icon_mixer},
+    {"MIXER", &mixer_page, 1, 0, 24, 16, (uint8_t *)icon_mixer},
     {"ROUTE", &route_page, 2, 0, 24, 16, (uint8_t *)icon_route},
-    {"LFO", &lfo_page, 3, 0, 24, 25, (uint8_t *)icon_lfo},
+    {"LFO", &lfo_page, 3, 0, 24, 24, (uint8_t *)icon_lfo},
 
     {"STEP EDIT", &seq_step_page, 4, 1, 24, 25, (uint8_t *)icon_step},
     {"RECORD", &seq_rtrk_page, 5, 1, 24, 15, (uint8_t*) icon_rec},
@@ -37,8 +37,8 @@ const PageSelectEntry Entries[] PROGMEM = {
     {"CHROMA", &seq_ptc_page, 7, 1, 24, 25, (uint8_t *)icon_chroma},
 
     {"SOUND MANAGER", &sound_browser, 8, 2, 0, 0, nullptr},
-    {"WAV DESIGNER", &wd.pages[0], 9, 2, 0, 0, nullptr},
-    {"LOUDNESS", &loudness_page, 10, 2, 0, 0, nullptr},
+    {"WAV DESIGNER", &wd.pages[0], 9, 2, 24, 19, (uint8_t*)icon_wavd},
+    {"LOUDNESS", &loudness_page, 10, 2, 24, 16, (uint8_t*)icon_loudness},
 
     {"DELAY", &fx_page_a, 12, 3, 24, 25, (uint8_t *)icon_rhytmecho},
     {"REVERB", &fx_page_b, 13, 3, 24, 25, (uint8_t *)icon_gatebox},
