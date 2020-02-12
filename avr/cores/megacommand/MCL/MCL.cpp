e37e8adde8192c83800ddb8afa875d4ddae5d6c0
Justin Mammarella <jmamma@gmail.com>
2020-02-12 15:47:11 +1100

    turn on track select listener

--- avr/cores/megacommand/MCL/MCL.cpp
+++ avr/cores/megacommand/MCL/MCL.cpp
@@ -47,6 +47,7 @@ void MCL::setup() {
   MDSysexListener.setup(&Midi);
   trig_interface.setup(&Midi);
   md_track_select.setup(&Midi);
+  md_track_select.on();
 #ifdef EXT_TRACKS
   A4SysexListener.setup(&Midi2);
 #endif
