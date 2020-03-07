93cbac6f6b58e35c639e2c72ef9b427031abc891
Yatao Li <yatli@microsoft.com>
2020-03-07 23:20:38 +0800

    fix project browser

--- avr/cores/megacommand/MCL/ProjectPages.cpp
+++ avr/cores/megacommand/MCL/ProjectPages.cpp
@@ -2,6 +2,7 @@
 #include "MCL.h"
 
 MCLEncoder loadproj_param1(0, 64, ENCODER_RES_SYS);
+MCLEncoder loadproj_param2(0, 64, ENCODER_RES_SYS);
 
-LoadProjectPage load_proj_page(&loadproj_param1,&loadproj_param1);
+LoadProjectPage load_proj_page(&loadproj_param1,&loadproj_param2);
 
