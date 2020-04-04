5e8d12b39eae9ab626df5764665aefa55a6b12cd
Yatao Li <yatli@microsoft.com>
2020-04-04 17:37:10 +0800

    update linux build script

--- avr/cores/megacommand/make_and_upload.sh
+++ avr/cores/megacommand/make_and_upload.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-AVR_DIR=$(cd "../../../../tools/avr"; pwd)
-DEV=/dev/$(ls /dev | grep usb | head -n 1)
-make -j8
-if [ $? -eq 0 ]; then
-  ${AVR_DIR}/bin/avrdude -C${AVR_DIR}/etc/avrdude.conf -v -V -patmega2560 -cwiring -P${DEV} -b115200 -D -Uflash:w:./main.hex
+# stop on first error
+set -e
+if [ ! -d "../../../../tools/avr" ]; then
+  AVR_DUDE="sudo avrdude"
+  AVR_DUDE_CONF=/etc/avrdude.conf
+  DEV=/dev/$(ls /dev | grep ttyUSB | head -n 1)
+else
+  AVR_DIR=$(cd "../../../../tools/avr"; pwd)
+  AVR_DUDE=${AVR_DIR}/bin/avrdude
+  AVR_DUDE_CONF=${AVR_DIR}/etc/avrdude.conf
+  DEV=/dev/$(ls /dev | grep usb | head -n 1)
 fi
+
+make -j8
+${AVR_DUDE} -C${AVR_DUDE_CONF} -v -V -patmega2560 -cwiring -P${DEV} -b115200 -D -Uflash:w:./main.hex
