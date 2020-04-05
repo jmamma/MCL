d0a92f51bae86c8dc2bb03ba0924438522a43656
Justin Mammarella <jmamma@gmail.com>
2020-04-05 14:11:28 +1000

    Draw sample position

--- avr/cores/megacommand/MCL/WavEditPage.h
+++ avr/cores/megacommand/MCL/WavEditPage.h
@@ -6,31 +6,34 @@
 #include "GUI.h"
 #include "Wav.h"
 
-#define WAV_DRAW_WIDTH 128
+#define WAV_DRAW_WIDTH 96
 #define WAV_DRAW_HEIGHT 32
-#define WAV_SECONDS 1.0 //maximmum sample length to display on screen
+#define WAV_SECONDS 1.0 // maximmum sample length to display on screen
 
 class WavEditPage : public LightPage {
 public:
   Wav wav_file;
   uint32_t start;
   uint32_t end;
-  uint32_t offset;
+  int32_t offset;
   uint32_t samples_per_pixel;
   int8_t wav_buf[WAV_DRAW_WIDTH][2];
   WavEditPage(Encoder *e1 = NULL, Encoder *e2 = NULL, Encoder *e3 = NULL,
-          Encoder *e4 = NULL)
-      : LightPage(e1, e2, e3, e4) {
-  }
+              Encoder *e4 = NULL)
+      : LightPage(e1, e2, e3, e4) {}
   virtual bool handleEvent(gui_event_t *event);
   void display();
   void setup();
   void open(char *file);
   void init();
   void loop();
-  void render(uint32_t sample_start, uint32_t sample_end, int32_t offset, uint32_t samples_per_pixel);
+  void render(uint32_t sample_start, uint32_t sample_end, int32_t sample_offset,
+              uint32_t samples_per_pixel);
   void cleanup();
   void draw_wav();
+
+  wav_sample_t get_selection_sample_start();
+  wav_sample_t get_selection_sample_end();
 };
 
 extern WavEditPage wav_edit_page;
