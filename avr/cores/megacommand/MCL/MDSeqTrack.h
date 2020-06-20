ce7c2925c3019209567195752683565422ce7309
Justin Mammarella <jmamma@gmail.com>
2020-06-20 20:49:49 +1000

    Forgot to checkin

--- avr/cores/megacommand/MCL/MDSeqTrack.h
+++ avr/cores/megacommand/MCL/MDSeqTrack.h
@@ -127,7 +127,7 @@ public:
   void send_trig();
   ALWAYS_INLINE() void send_trig_inline();
   ALWAYS_INLINE() bool trig_conditional(uint8_t condition);
-  ALWAYS_INLINE() void send_parameter_locks(uint8_t step);
+  ALWAYS_INLINE() void send_parameter_locks(uint8_t step, bool pattern_mask_step);
 
   ALWAYS_INLINE() void send_slides();
   ALWAYS_INLINE() void recalc_slides();
