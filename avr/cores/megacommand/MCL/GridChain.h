c79b549455797bf3aab907d6e144f534bd633032
Justin Mammarella <jmamma@gmail.com>
2020-09-20 19:31:11 +1000

    GridChain object not correctly initialized

--- avr/cores/megacommand/MCL/GridChain.h
+++ avr/cores/megacommand/MCL/GridChain.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "SeqTrack.h"
 
 class GridChain_270 {
 
@@ -23,8 +24,12 @@ public:
   uint8_t loops;
   uint8_t length;
   uint8_t speed;
-  GridChain(uint8_t active_ = 0, uint8_t row_ = 0, uint8_t col_ = 0,
-            uint8_t loops_ = 0) {}
+  GridChain() {
+  row = 0;
+  loops = 0;
+  length = 16;
+  speed = SEQ_SPEED_1X;
+  }
   //Store chain data in chain array.
   void store_in_mem(uint8_t tracknumber, GridChain *chain_array) {
      memcpy(&chain_array[tracknumber], this, sizeof(GridChain));
