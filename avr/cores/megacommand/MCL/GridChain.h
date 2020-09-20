5ae50a5c37fc64ae7c87925e36951804942e5172
Justin Mammarella <jmamma@gmail.com>
2020-09-20 19:34:36 +1000

    Revert "GridChain object not correctly initialized"
    Don't do this, constructor gets called on load and blows away chain
    settings.
    
    This reverts commit c79b549455797bf3aab907d6e144f534bd633032.

--- avr/cores/megacommand/MCL/GridChain.h
+++ avr/cores/megacommand/MCL/GridChain.h
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include "SeqTrack.h"
 
 class GridChain_270 {
 
@@ -24,12 +23,8 @@ public:
   uint8_t loops;
   uint8_t length;
   uint8_t speed;
-  GridChain() {
-  row = 0;
-  loops = 0;
-  length = 16;
-  speed = SEQ_SPEED_1X;
-  }
+  GridChain(uint8_t active_ = 0, uint8_t row_ = 0, uint8_t col_ = 0,
+            uint8_t loops_ = 0) {}
   //Store chain data in chain array.
   void store_in_mem(uint8_t tracknumber, GridChain *chain_array) {
      memcpy(&chain_array[tracknumber], this, sizeof(GridChain));
