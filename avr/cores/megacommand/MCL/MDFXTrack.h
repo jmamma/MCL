f9339caa4575eb564fbd55eea8608bd187518fd3
Justin Mammarella <jmamma@gmail.com>
2020-09-22 21:49:52 +1000

    Consoldiate MDFXTrack.h and MDFXTrackData.h

--- avr/cores/megacommand/MCL/MDFXTrack.h
+++ avr/cores/megacommand/MCL/MDFXTrack.h
@@ -3,7 +3,22 @@
 #pragma once
 
 #include "DeviceTrack.h"
-#include "MDFXData.h"
+
+class MDFXData {
+public:
+  bool enable_reverb;
+  bool enable_delay;
+  bool enable_eq;
+  bool enable_dynamics;
+  /** The settings of the reverb effect. f**/
+  uint8_t reverb[8];
+  /** The settings of the delay effect. **/
+  uint8_t delay[8];
+  /** The settings of the EQ effect. **/
+  uint8_t eq[8];
+  /** The settings of the compressor effect. **/
+  uint8_t dynamics[8];
+};
 
 class MDFXTrack : public DeviceTrack, public MDFXData {
 public:
