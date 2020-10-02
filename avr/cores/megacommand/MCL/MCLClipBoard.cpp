c3507005e0a3b681d48905240c8500972f233121
Justin Mammarella <jmamma@gmail.com>
2020-10-02 23:27:46 +1000

    Machine wasn't transmitted on paste

--- avr/cores/megacommand/MCL/MCLClipBoard.cpp
+++ avr/cores/megacommand/MCL/MCLClipBoard.cpp
@@ -161,7 +161,7 @@ bool MCLClipBoard::paste_sequencer_track(uint8_t source_track, uint8_t track) {
       md_track->machine.lfo.destinationTrack = track;
     }
     DEBUG_PRINTLN(F("sending seq track"));
-    bool send_machine, send_level = true;
+    bool send_machine = true, send_level = true;
     MD.sendMachine(track, &(md_track->machine), send_level, send_machine);
   }
 #ifdef EXT_TRACKS
