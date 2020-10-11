cfb6310b16fafeb4d26b8673edac2f3bcc474d5c
Justin Mammarella <jmamma@gmail.com>
2020-10-11 22:22:39 +1100

    regression: update menu ordering

--- avr/cores/megacommand/MCL/SeqPages.h
+++ avr/cores/megacommand/MCL/SeqPages.h
@@ -19,18 +19,19 @@
 #define NUM_PARAM_PAGES 4
 
 #define SEQ_MENU_MASK 0
-#define SEQ_MENU_ARP 1
-#define SEQ_MENU_TRANSPOSE 2
-#define SEQ_MENU_TRACK 3
-#define SEQ_MENU_LENGTH 4
-#define SEQ_MENU_CHANNEL 5
-#define SEQ_MENU_COPY 6
-#define SEQ_MENU_CLEAR_TRACK 7
-#define SEQ_MENU_CLEAR_LOCKS 8
-#define SEQ_MENU_PASTE 9
+#define SEQ_MENU_TRACK 1
+#define SEQ_MENU_ARP 2
+#define SEQ_MENU_TRANSPOSE 3
+#define SEQ_MENU_COPY 4
+#define SEQ_MENU_CLEAR_TRACK 5
+#define SEQ_MENU_CLEAR_LOCKS 6
+#define SEQ_MENU_PASTE 7
+#define SEQ_MENU_SPEED 8
+#define SEQ_MENU_LENGTH 9
 #define SEQ_MENU_SPEED 10
-#define SEQ_MENU_SHIFT 11
-#define SEQ_MENU_REVERSE 12
+#define SEQ_MENU_CHANNEL 11
+#define SEQ_MENU_SHIFT 12
+#define SEQ_MENU_REVERSE 13
 
 extern MCLEncoder seq_param1;
 extern MCLEncoder seq_param2;
