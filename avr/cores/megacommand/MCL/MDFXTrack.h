e805925198b260454a42c2043e8c10aab1704de6
Justin Mammarella <jmamma@gmail.com>
2020-12-04 23:38:45 +1100

    some fixes for chain mode

--- avr/cores/megacommand/MCL/MDFXTrack.h
+++ avr/cores/megacommand/MCL/MDFXTrack.h
@@ -53,6 +53,6 @@ public:
   virtual uint8_t get_model() { return MDFX_TRACK_TYPE; }
   virtual uint8_t get_device_type() { return MDFX_TRACK_TYPE; }
 
-  virtual void* get_sound_data_ptr() { return nullptr; }
-  virtual size_t get_sound_data_size() { return 0; }
+  virtual void* get_sound_data_ptr() { return &reverb; }
+  virtual size_t get_sound_data_size() { return 8 * 4; }
 };
