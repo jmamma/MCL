9a3eb2313127c7b26fdeed5a2320eb42f18f978d
Justin Mammarella <jmamma@gmail.com>
2020-12-05 23:53:15 +1100

    Revert "fix conversion page garbled text"
    This reverts commit d3967bb1734402bc198067964ed53e7c82322819.

--- avr/cores/megacommand/MCL/ProjectPages.cpp
+++ avr/cores/megacommand/MCL/ProjectPages.cpp
@@ -4,5 +4,5 @@ MCLEncoder loadproj_param1(0, 64, ENCODER_RES_SYS);
 
 LoadProjectPage load_proj_page(&loadproj_param1,&loadproj_param1);
 
-ConvertProjectPage convert_proj_page(&loadproj_param1,&loadproj_param2);
+ConvertProjectPage convert_proj_page(&loadproj_param1,&loadproj_param1);
 
