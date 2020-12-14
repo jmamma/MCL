21fc774c46c2e4c9b7947751b4322e2f29f974e6
Justin Mammarella <jmamma@gmail.com>
2020-12-14 15:36:33 +1100

    WavDesigner new UI + menu

---
+++ avr/cores/megacommand/MCL/WavDesignerPage.cpp
@@ -0,0 +1,101 @@
+#include "MCL_impl.h"
+
+uint8_t WavDesignerPage::opt_mode = 0;
+uint8_t WavDesignerPage::opt_shape = 0;
+bool WavDesignerPage::show_menu = false;
+uint8_t WavDesignerPage::last_mode = 0;
+
+MCLEncoder *WavDesignerPage::opt_param1_capture;
+MCLEncoder *WavDesignerPage::opt_param2_capture;
+
+void wav_menu_handler() {
+  if (WavDesignerPage::opt_mode == WavDesignerPage::last_mode) {
+    if (WavDesignerPage::opt_mode == 0) {
+      wd.pages[WavDesignerPage::opt_mode].osc_waveform =
+          WavDesignerPage::opt_shape;
+    }
+  }
+  WavDesignerPage::last_mode = WavDesignerPage::opt_mode;
+  if (WavDesignerPage::opt_mode == 3) {
+    wav_menu_page.menu.enable_entry(1, false);
+    wav_menu_page.menu.enable_entry(2, true);
+    GUI.setPage(&wd.mixer);
+  } else {
+    wav_menu_page.menu.enable_entry(1, true);
+    wav_menu_page.menu.enable_entry(2, false);
+    GUI.setPage(&wd.pages[WavDesignerPage::opt_mode]);
+  }
+}
+
+void wav_render() { wd.prompt_send(); }
+
+const menu_t<3> wav_menu_layout PROGMEM = {
+    "",
+    {
+        {"EDIT:", 0, 4, 4, (uint8_t *)&WavDesignerPage::opt_mode, (Page *)NULL,
+         NULL, 75},
+        {"WAV:", 0, 6, 6, (uint8_t *)&WavDesignerPage::opt_shape, (Page *)NULL,
+         NULL, 69},
+        {"RENDER", 0, 0, 0, (uint8_t *)NULL, (Page *)NULL, wav_render, 0},
+    },
+    wav_menu_handler,
+};
+
+MCLEncoder wav_menu_value_encoder(0, 16, ENCODER_RES_PAT);
+MCLEncoder wav_menu_entry_encoder(0, 4, ENCODER_RES_PAT);
+MenuPage<3> wav_menu_page(&wav_menu_layout, &wav_menu_value_encoder,
+                          &wav_menu_entry_encoder);
+
+void WavDesignerPage::display() {
+  if (show_menu) {
+    constexpr uint8_t width = 52;
+    oled_display.setFont(&TomThumb);
+    oled_display.fillRect(128 - width - 2, 0, width + 2, 32, BLACK);
+    wav_menu_page.draw_menu(128 - width, 8, width);
+  }
+}
+
+void WavDesignerPage::loop() {
+  if (show_menu) {
+    wav_menu_page.loop();
+  }
+}
+
+bool WavDesignerPage::handleEvent(gui_event_t *event) {
+  if (EVENT_PRESSED(event, Buttons.BUTTON3)) {
+    opt_param1_capture = (MCLEncoder *)encoders[0];
+    opt_param2_capture = (MCLEncoder *)encoders[1];
+    encoders[0] = &wav_menu_value_encoder;
+    encoders[1] = &wav_menu_entry_encoder;
+    show_menu = true;
+    if (WavDesignerPage::opt_mode < 3) {
+      WavDesignerPage::opt_shape =
+          wd.pages[WavDesignerPage::opt_mode].osc_waveform;
+    }
+    wav_menu_page.init();
+    return true;
+  }
+  if (EVENT_RELEASED(event, Buttons.BUTTON3)) {
+    if (show_menu) {
+      void (*row_func)();
+      row_func =
+          wav_menu_page.menu.get_row_function(wav_menu_page.encoders[1]->cur);
+
+      show_menu = false;
+      encoders[0] = (Encoder *)opt_param1_capture;
+      encoders[1] = (Encoder *)opt_param2_capture;
+
+      if (row_func != NULL) {
+        row_func();
+      } else {
+        wav_menu_handler();
+      }
+    }
+    return true;
+  }
+  if (EVENT_PRESSED(event, Buttons.BUTTON2)) {
+    GUI.setPage(&page_select_page);
+    return true;
+  }
+  return false;
+}
---
+++ avr/cores/megacommand/MCL/WavDesignerPage.h
@@ -0,0 +1,38 @@
+/* Copyright Justin Mammarella jmamma@gmail.com 2018 */
+
+#ifndef WAVDESIGNERPAGE_H__
+#define WAVDESIGNERPAGE_H__
+
+#include "MCL.h"
+
+extern void wav_menu_handler();
+extern MCLEncoder wav_menu_value_encoder;
+extern MCLEncoder wav_menu_entry_encoder;
+extern MenuPage<3> wav_menu_page;
+
+class WavDesignerPage : public LightPage {
+  public:
+  uint8_t id;
+  static uint8_t opt_mode;
+  static uint8_t opt_shape;
+  static uint8_t last_mode;
+
+  static bool show_menu;
+
+  static MCLEncoder *opt_param1_capture;
+  static MCLEncoder *opt_param2_capture;
+  WavDesignerPage(Encoder *e1 = NULL, Encoder *e2 = NULL, Encoder *e3 = NULL,
+            Encoder *e4 = NULL)
+      : LightPage(e1, e2, e3, e4) {
+      }
+
+  virtual void init() {
+  wav_menu_page.menu.enable_entry(2, false);
+  show_menu = false;
+  }
+  virtual void loop();
+  virtual void display();
+  virtual bool handleEvent(gui_event_t *event);
+};
+
+#endif /* WAVDESIGNER_H__ */
