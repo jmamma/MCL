9fe32e0dc7f053681db6a6c00cae70dc03c64cdd
Justin Mammarella <jmamma@gmail.com>
2021-05-12 22:56:07 +1000

    sync lfo cursor

--- avr/cores/megacommand/MCL/LFOPage.cpp
+++ avr/cores/megacommand/MCL/LFOPage.cpp
@@ -27,10 +27,12 @@ void LFOPage::init() {
   oled_display.setFont();
 #endif
   update_encoders();
-
+  MD.sync_seqtrack(lfo_track->length, lfo_track->speed,
+                     lfo_track->step_count);
   if (lfo_track->mode != LFO_MODE_FREE) {
     trig_interface.on();
   }
+
   // LFOPage not using base SeqPage init?
   R.Clear();
   R.use_machine_param_names();
