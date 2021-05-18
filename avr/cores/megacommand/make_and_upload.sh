f7adfc1a93e6912d72651a42ed30dfbc4dd2d8ac
Justin Mammarella <jmamma@gmail.com>
2021-05-18 12:56:36 +1000

    show RAM heavy hitters

--- avr/cores/megacommand/make_and_upload.sh
+++ avr/cores/megacommand/make_and_upload.sh
@@ -2,7 +2,9 @@
 AVR_DIR=$(cd "../../../../tools/avr"; pwd)
 DEV=/dev/$(ls /dev | grep usb | grep cu | tail -n 1)
 make -j8
+
 if [ $? -eq 0 ]; then
+  ${AVR_DIR}/bin/avr-nm main.elf -Crtd --size-sort | grep -i ' [dbv] ' | sort | tail -n 20
   ram_used=$(${AVR_DIR}/bin/avr-size main.elf | grep main | awk '{ print $2 + $3}')
   ram_free=$((1024 * 64 - $ram_used - 8 * 1024))
   echo RAM_USED: $ram_used
