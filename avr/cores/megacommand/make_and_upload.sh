a8c6b45afbb2462168c98add974552debd7e1d6a
Justin Mammarella <jmamma@gmail.com>
2021-05-18 11:40:27 +1000

    ram free calc

--- avr/cores/megacommand/make_and_upload.sh
+++ avr/cores/megacommand/make_and_upload.sh
@@ -3,6 +3,9 @@ AVR_DIR=$(cd "../../../../tools/avr"; pwd)
 DEV=/dev/$(ls /dev | grep usb | grep cu | tail -n 1)
 make -j8
 if [ $? -eq 0 ]; then
-  echo RAM USAGE: $(${AVR_DIR}/bin/avr-size main.elf | grep main | awk '{ print $2 + $3}')
+  ram_used=$(${AVR_DIR}/bin/avr-size main.elf | grep main | awk '{ print $2 + $3}')
+  ram_free=$((1024 * 64 - $ram_used - 8 * 1024))
+  echo RAM_USED: $ram_used
+  echo RAM_FREE: $ram_free
   ${AVR_DIR}/bin/avrdude -C${AVR_DIR}/etc/avrdude.conf -v -V -patmega2560 -cwiring -P${DEV} -b115200 -D -Uflash:w:./main.hex
 fi
