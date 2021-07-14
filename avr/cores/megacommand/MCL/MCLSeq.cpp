1262aa509445c180c9e78fcf21d39821cad8ba4d
Justin Mammarella <jmamma@gmail.com>
2021-07-14 23:50:15 +1000

    dont reset mute_state on stop

--- avr/cores/megacommand/MCL/MCLSeq.cpp
+++ avr/cores/megacommand/MCL/MCLSeq.cpp
@@ -153,7 +153,6 @@ void MCLSeq::onMidiStopCallback() {
   }
 #endif
   for (uint8_t i = 0; i < num_md_tracks; i++) {
-    md_tracks[i].mute_state = SEQ_MUTE_OFF;
     md_tracks[i].reset_params();
     md_tracks[i].locks_slides_recalc = 255;
     for (uint8_t c = 0; c < NUM_LOCKS; c++) {
