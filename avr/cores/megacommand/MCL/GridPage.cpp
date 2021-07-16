f44c65e22b10139b72b13e136a382616bdb5272e
Justin Mammarella <jmamma@gmail.com>
2021-07-16 19:01:36 +1000

    typo

--- avr/cores/megacommand/MCL/GridPage.cpp
+++ avr/cores/megacommand/MCL/GridPage.cpp
@@ -75,7 +75,7 @@ void GridPage::close_bank_popup() {
   bank_popup = 0;
   note_interface.init_notes();
   //Clear blink leds
-  MD.set_trigleds(0 TRIGLED_EXCLUSIVENDYNAMIC, 1);
+  MD.set_trigleds(0, TRIGLED_EXCLUSIVENDYNAMIC, 1);
 }
 
 void GridPage::loop() {
