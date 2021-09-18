9e9a0c61f0f472d3edae18edbc7cdb0e16e2c175
Justin Mammarella <jmamma@gmail.com>
2021-09-18 21:08:00 +1000

    4.02, changelog

--- avr/cores/megacommand/MCL/MCL.h
+++ avr/cores/megacommand/MCL/MCL.h
@@ -18,8 +18,8 @@
 #include "Fonts/Elektrothic.h"
 #endif
 
-#define VERSION 4001
-#define VERSION_STR "4.01"
+#define VERSION 4002
+#define VERSION_STR "4.02"
 
 #define CALLBACK_TIMEOUT 500
 #define GUI_NAME_TIMEOUT 800
