85e2cf84e889777d0ff448692a39b009dee68142
Justin Mammarella <jmamma@gmail.com>
2022-03-28 21:31:13 +1100

    minor

--- avr/cores/megacommand/MCL/SeqExtStepPage.cpp
+++ avr/cores/megacommand/MCL/SeqExtStepPage.cpp
@@ -794,7 +794,7 @@ bool SeqExtStepPage::handleEvent(gui_event_t *event) {
     int w = timing_mid;
     if (trig_interface.is_key_down(MDX_KEY_FUNC)) {
       inc = 4;
-      w = w * 4;
+      w = w * 2;
     }
 
     if (pianoroll_mode > 0) {
