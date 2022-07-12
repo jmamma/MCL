980f7483a16240a99163d55330ec9170a9ee43de
Justin Mammarella <jmamma@gmail.com>
2022-07-12 14:38:10 +1000

    increase to 5s

--- avr/cores/megacommand/MCL/MidiActivePeering.cpp
+++ avr/cores/megacommand/MCL/MidiActivePeering.cpp
@@ -178,7 +178,7 @@ void MidiActivePeering::run() {
   resource_loaded = false;
 
   //Setting USB turbo speed too early can cause OS upload to fail
-  if (turbo_light.lookup_speed(mcl_cfg.usb_turbo) != MidiUartUSB.speed && slowclock > 2000 && usb_set_speed) {
+  if (turbo_light.lookup_speed(mcl_cfg.usb_turbo) != MidiUartUSB.speed && slowclock > 5000 && usb_set_speed) {
      turbo_light.set_speed(turbo_light.lookup_speed(mcl_cfg.usb_turbo), MidiUSB.uart);
      usb_set_speed = false;
   }
