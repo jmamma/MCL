3438bb51a131a3f399d616014d17b8f5466e428a
Justin Mammarella <jmamma@gmail.com>
2023-02-02 21:29:06 +1100

    bug, link loading missing

--- avr/cores/megacommand/MCL/MDLFOTrack.cpp
+++ avr/cores/megacommand/MCL/MDLFOTrack.cpp
@@ -3,19 +3,19 @@ void MDLFOTrack::transition_send(uint8_t tracknumber, uint8_t slotnumber) {}
 
 void MDLFOTrack::transition_load(uint8_t tracknumber, SeqTrack *seq_track,
                                   uint8_t slotnumber) {
-  load_link_data(seq_track);
   GridTrack::transition_load(tracknumber, seq_track, slotnumber);
   memcpy(mcl_seq.lfo_tracks[0].data(), lfo_data.data(),
          sizeof(LFOSeqTrackData));
+  load_link_data(seq_track);
 }
 
 uint16_t MDLFOTrack::calc_latency(uint8_t tracknumber) { return 0; }
 
 void MDLFOTrack::load_immediate(uint8_t tracknumber, SeqTrack *seq_track) {
-  load_link_data(seq_track);
   memcpy(mcl_seq.lfo_tracks[0].data(), lfo_data.data(),
          sizeof(LFOSeqTrackData));
-
+  DEBUG_PRINTLN("MD LFO LOAD IMMI");
+  load_link_data(seq_track);
   DEBUG_PRINTLN("Load speed");
   DEBUG_PRINTLN(lfo_data.speed);
 }
