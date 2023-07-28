b0dd27abc7596a59e929784d7c2ee44eb9429006
Justin Mammarella <jmamma@gmail.com>
2023-07-28 11:48:10 +1000

    optimise

--- avr/cores/megacommand/MCL/GridRowHeader.cpp
+++ avr/cores/megacommand/MCL/GridRowHeader.cpp
@@ -18,10 +18,13 @@ bool GridRowHeader::is_empty() {
 }
 
 void GridRowHeader::init() {
+  memset(this, 0, sizeof(GridRowHeader));
+  /*
   active = false;
   for (uint8_t x = 0; x < GRID_WIDTH; x++) {
     track_type[x] = EMPTY_TRACK_TYPE;
     model[x] = 0;
   }
+  */
 }
 
