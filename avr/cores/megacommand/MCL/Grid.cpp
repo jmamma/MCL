dd5c6fe0d6dc0b46dd293a90881bed90c5d62d66
Justin Mammarella <jmamma@gmail.com>
2023-10-12 11:01:41 +1100

    minor

--- avr/cores/megacommand/MCL/Grid.cpp
+++ avr/cores/megacommand/MCL/Grid.cpp
@@ -80,7 +80,7 @@ bool Grid::new_grid(const char *gridname) {
   oled_display.drawRect(15, 23, 98, 6, WHITE);
 #endif
   // Initialise the project file by filling the grid with blank data.
-  uint8_t ledstatus;
+  uint8_t ledstatus = 0;
   for (uint16_t i = 0; i < GRID_LENGTH; i++) {
 
 #ifdef OLED_DISPLAY
