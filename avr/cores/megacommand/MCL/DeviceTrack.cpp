8426276b812c615fe20a549c63967cc85c83fb41
Justin Mammarella <jmamma@gmail.com>
2024-10-23 21:28:29 +1100

    GridChainTrack not used

--- avr/cores/megacommand/MCL/DeviceTrack.cpp
+++ avr/cores/megacommand/MCL/DeviceTrack.cpp
@@ -31,9 +31,9 @@ DeviceTrack *DeviceTrack::init_track_type(uint8_t track_type) {
   case MNM_TRACK_TYPE:
     ::new (this) MNMTrack;
     break;
-   case GRIDCHAIN_TRACK_TYPE:
-    ::new (this) GridChainTrack;
-    break;
+//  case GRIDCHAIN_TRACK_TYPE:
+//    ::new (this) GridChainTrack;
+//    break;
    case PERF_TRACK_TYPE:
     ::new (this) PerfTrack;
     break;
