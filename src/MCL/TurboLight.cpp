00ff84491132523732b4c0cbf067b1a5f4154c78
Justin Mammarella <jmamma@gmail.com>
2024-12-22 19:33:57 +1100

    This check should not be needed. m_putc_immediate is blocking

--- src/MCL/TurboLight.cpp
+++ src/MCL/TurboLight.cpp
@@ -37,7 +37,7 @@ void TurboLight::set_speed(uint8_t speed, MidiUartClass *uart) {
 
   uart->m_putc_immediate(0xF7);
 
-  while (!uart->check_empty_tx());
+ // while (!uart->check_empty_tx());
 
   for (uint8_t n = 0; n < 16; n++) {
     uart->m_putc_immediate(0x00);
