68f9ff9ed5f47dfe19ad6b526ecf19ce142de892
Justin Mammarella <jmamma@gmail.com>
2025-07-24 08:57:34 +1000

    fix inlining = avr bloat

--- src/platform/avr/platform.h
+++ src/platform/avr/platform.h
@@ -39,10 +39,10 @@ extern volatile uint32_t interrupt_lock_count;
 #define sleep_ms(x) delay(x)
 
 // Function inlining configuration
-#if defined(PLATFORM_MEGACOMMAND) && defined(IS_ISR_ROUTINE)
+#if defined(MEGACOMMAND) && defined(IS_ISR_ROUTINE)
     #define ALWAYS_INLINE() __attribute__((always_inline))
     #define FORCED_INLINE() __attribute__((always_inline))
-#elif defined(PLATFORM_MEGACOMMAND)
+#elif defined(MEGACOMMAND)
     #define ALWAYS_INLINE()
     #define FORCED_INLINE() __attribute__((always_inline))
 #else
