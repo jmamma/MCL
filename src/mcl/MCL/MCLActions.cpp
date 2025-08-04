6f6b774936d54a6a339076014a66f64ae5e709e3
Justin Mammarella <jmamma@gmail.com>
2025-08-04 22:12:04 +1000

    Fix lockup condition

--- src/mcl/MCL/MCLActions.cpp
+++ src/mcl/MCL/MCLActions.cpp
@@ -771,8 +771,8 @@ void MCLActions::cache_next_tracks(uint8_t *slot_select_array,
         }
       } else if (chains[n].get_length() != QUANT_LEN) {
         links[n].loops = 1;
-        links[n].length = (float)chains[n].get_length() /
-                          (float)gdt->seq_track->get_speed_multiplier();
+        links[n].length = max(1,(float)chains[n].get_length() /
+                          (float)gdt->seq_track->get_speed_multiplier());
         while (links[n].loops * links[n].length < 8) {
           links[n].loops++;
         }
