bd8f43bfe5b44c0cab4c7804032ed52783a8554e
Justin Mammarella <jmamma@gmail.com>
2026-05-09 00:04:41 +1000

    Devirtualize MIDI peering helpers

--- src/mcl/MCL/MidiActivePeering.h
+++ src/mcl/MCL/MidiActivePeering.h
@@ -12,11 +12,10 @@ public:
   MidiActivePeering(uint16_t _interval = 250) : Task(_interval) {
     setup(_interval);
   }
-  virtual void setup(uint16_t _interval = 250) { interval = _interval; }
-  virtual void disconnect(uint8_t port);
-  virtual void force_connect(uint8_t port, MidiDevice *driver);
+  void setup(uint16_t _interval = 250) { interval = _interval; }
+  void disconnect(uint8_t port);
+  void force_connect(uint8_t port, MidiDevice *driver);
   virtual void run();
-  virtual void destroy(){};
 };
 
 extern MidiActivePeering midi_active_peering;
