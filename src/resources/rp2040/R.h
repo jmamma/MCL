5f96e3b1b680dedeaf952bcf4fc171ddbb658bd7
Justin Mammarella <jmamma@gmail.com>
2026-05-26 16:15:09 +1000

    checkin resources

--- src/resources/rp2040/R.h
+++ src/resources/rp2040/R.h
@@ -507,10 +507,10 @@ extern const unsigned char __R_menu_options[] PROGMEM;
 struct __T_menu_options {
   union {
     menu_option_t MENU_OPTIONS[0];
-    char zz__MENU_OPTIONS[1460];
+    char zz__MENU_OPTIONS[1368];
   };
-  static constexpr size_t countof_MENU_OPTIONS = 1460 / sizeof(menu_option_t);
-  static constexpr size_t sizeofof_MENU_OPTIONS = 1460;
-  static constexpr size_t __total_size = 1460;
+  static constexpr size_t countof_MENU_OPTIONS = 1368 / sizeof(menu_option_t);
+  static constexpr size_t sizeofof_MENU_OPTIONS = 1368;
+  static constexpr size_t __total_size = 1368;
 };
 
