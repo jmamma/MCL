fcc4a24e18701b27c51ec0fb062eb7476fea2ce3
Justin Mammarella <jmamma@gmail.com>
2026-06-07 20:42:53 +1000

    Reduce AVR menu layout restore flash use

--- src/mcl/ResourceManager/ResourceManager.cpp
+++ src/mcl/ResourceManager/ResourceManager.cpp
@@ -1,5 +1,73 @@
 #include "ResourceManager.h"
 #include "MCLMenus.h"
+#include <stddef.h>
+
+#if defined(__AVR__)
+struct MenuLayoutBinding {
+  MenuBase *menu;
+  uint16_t layout_offset;
+  uint8_t entry_count;
+};
+
+#define MENU_LAYOUT_BINDING(page, layout, count) \
+  { &((page).menu), offsetof(__T_menu_layouts, layout), count }
+
+static const MenuLayoutBinding menu_layout_bindings[] PROGMEM = {
+  MENU_LAYOUT_BINDING(system_page, system_menu_layout, system_menu_page_N),
+  MENU_LAYOUT_BINDING(midi_config_page, midiconfig_menu_layout,
+                      midi_config_page_N),
+  MENU_LAYOUT_BINDING(md_config_page, mdconfig_menu_layout, md_config_page_N),
+  MENU_LAYOUT_BINDING(mcl_config_page, mclconfig_menu_layout,
+                      mcl_config_page_N),
+  MENU_LAYOUT_BINDING(file_menu_page, file_menu_layout, file_menu_page_N),
+  MENU_LAYOUT_BINDING(seq_menu_page, seq_menu_layout, seq_menu_page_N),
+  MENU_LAYOUT_BINDING(grid_slot_page, slot_menu_layout, grid_slot_page_N),
+  MENU_LAYOUT_BINDING(wavdesign_menu_page, wavdesign_menu_layout,
+                      wavdesign_menu_page_N),
+  MENU_LAYOUT_BINDING(md_import_page, mdimport_menu_layout, md_import_page_N),
+  MENU_LAYOUT_BINDING(start_menu_page, start_menu_layout, start_menu_page_N),
+  MENU_LAYOUT_BINDING(boot_menu_page, boot_menu_layout, boot_menu_page_N),
+  MENU_LAYOUT_BINDING(mididevice_menu_page, mididevice_menu_layout,
+                      mididevice_menu_page_N),
+  MENU_LAYOUT_BINDING(gridx_menu_page, gridx_menu_layout, gridx_menu_page_N),
+  MENU_LAYOUT_BINDING(gridy_menu_page, gridy_menu_layout, gridy_menu_page_N),
+  MENU_LAYOUT_BINDING(midiport_menu_page, midiport_menu_layout,
+                      midiport_menu_page_N),
+  MENU_LAYOUT_BINDING(port1_menu_page, port1_menu_layout, port1_menu_page_N),
+  MENU_LAYOUT_BINDING(port2_menu_page, port2_menu_layout, port2_menu_page_N),
+  MENU_LAYOUT_BINDING(usbport_menu_page, usbport_menu_layout,
+                      usbport_menu_page_N),
+  MENU_LAYOUT_BINDING(midiprogram_menu_page, midiprogram_menu_layout,
+                      midiprogram_menu_page_N),
+  MENU_LAYOUT_BINDING(midiclock_menu_page, midiclock_menu_layout,
+                      midiclock_menu_page_N),
+  MENU_LAYOUT_BINDING(midiroute_menu_page, midiroute_menu_layout,
+                      midiroute_menu_page_N),
+  MENU_LAYOUT_BINDING(midimachinedrum_menu_page,
+                      midimachinedrum_menu_layout,
+                      midimachinedrum_menu_page_N),
+  MENU_LAYOUT_BINDING(midigeneric_menu_page, midigeneric_menu_layout,
+                      midigeneric_menu_page_N),
+  MENU_LAYOUT_BINDING(midicontrolinput_menu_page,
+                      midicontrolinput_menu_layout,
+                      midicontrolinput_menu_page_N),
+  MENU_LAYOUT_BINDING(midicontroloutput_menu_page,
+                      midicontroloutput_menu_layout,
+                      midicontroloutput_menu_page_N),
+  MENU_LAYOUT_BINDING(perf_menu_page, perf_menu_layout, perf_menu_page_N),
+};
+
+static void set_menu_layout(MenuBase *menu, const void *layout,
+                            uint8_t entry_count) {
+  menu->layout_base = layout;
+  menu->entry_count = entry_count;
+  menu->exit_fn_id =
+      ((const uint8_t *)layout)[offsetof(menu_t<1>, items) +
+                                entry_count * sizeof(menu_item_t)];
+}
+
+#undef MENU_LAYOUT_BINDING
+#endif
 
 ResourceManager::ResourceManager() { }
 
@@ -69,6 +137,16 @@ size_t ResourceManager::Size() {
 }
 
 void ResourceManager::restore_menu_layout_deps() {
+#if defined(__AVR__)
+  const uint8_t *base = (const uint8_t *)R.menu_layouts;
+  for (uint8_t i = 0; i < countof(menu_layout_bindings); ++i) {
+    const MenuLayoutBinding *binding = &menu_layout_bindings[i];
+    MenuBase *menu = (MenuBase *)pgm_read_word(&binding->menu);
+    uint16_t layout_offset = pgm_read_word(&binding->layout_offset);
+    uint8_t entry_count = pgm_read_byte(&binding->entry_count);
+    set_menu_layout(menu, base + layout_offset, entry_count);
+  }
+#else
   system_page.set_layout(R.menu_layouts->system_menu_layout);
   midi_config_page.set_layout(R.menu_layouts->midiconfig_menu_layout);
   md_config_page.set_layout(R.menu_layouts->mdconfig_menu_layout);
@@ -95,6 +173,7 @@ void ResourceManager::restore_menu_layout_deps() {
   midicontrolinput_menu_page.set_layout(R.menu_layouts->midicontrolinput_menu_layout);
   midicontroloutput_menu_page.set_layout(R.menu_layouts->midicontroloutput_menu_layout);
   perf_menu_page.set_layout(R.menu_layouts->perf_menu_layout);
+#endif
 }
 
 ResourceManager R;
